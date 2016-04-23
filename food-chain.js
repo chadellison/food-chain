da2ff4bf8752a13e14561bffc0d41e358f1dc7f4
Chad Ellison <chad.ellison0123@gmail.com>
2016-04-23 14:03:48 -0600

    Add Js food-chain

---
+++ food-chain.js
@@ -0,0 +1,98 @@
+var FoodChain = function() {}
+
+FoodChain.prototype.verse = function(verse) {
+  switch (verse) {
+    case 1:
+    return verseOne() + ending()
+    break
+    case 2:
+    return verseTwo() + ending()
+    break
+    case 3:
+    return verseThree() + ending()
+    break
+    case 4:
+    return verseFour() + ending()
+    break
+    case 5:
+    return verseFive() + ending()
+    break
+    case 6:
+    return verseSix() + ending()
+    break
+    case 7:
+    return verseSeven() + ending()
+    break
+    case 8:
+    return verseEight()
+    break
+  }
+}
+
+FoodChain.prototype.verses = function(start, end) {
+  return this.verse(start) +'\n' + this.verse(end) + '\n' 
+}
+
+var verseOne = function() {
+  return 'I know an old lady who swallowed a fly.\n'
+}
+
+var verseTwo = function() {
+  return 'I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n' +
+    'She swallowed the spider to catch the fly.\n'
+}
+
+var verseThree = function() {
+  return 'I know an old lady who swallowed a bird.\n' +
+    'How absurd to swallow a bird!\n' +
+    'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' +
+    'She swallowed the spider to catch the fly.\n'
+}
+
+var verseFour = function() {
+  return 'I know an old lady who swallowed a cat.\n' +
+    'Imagine that, to swallow a cat!\n' +
+    'She swallowed the cat to catch the bird.\n' +
+    'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' +
+    'She swallowed the spider to catch the fly.\n'
+}
+
+var verseFive = function() {
+  return 'I know an old lady who swallowed a dog.\n' +
+    'What a hog, to swallow a dog!\n' +
+    'She swallowed the dog to catch the cat.\n' +
+    'She swallowed the cat to catch the bird.\n' +
+    'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' +
+    'She swallowed the spider to catch the fly.\n'
+}
+
+var verseSix = function() {
+  return 'I know an old lady who swallowed a goat.\n' +
+    'Just opened her throat and swallowed a goat!\n' +
+    'She swallowed the goat to catch the dog.\n' +
+    'She swallowed the dog to catch the cat.\n' +
+    'She swallowed the cat to catch the bird.\n' +
+    'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' +
+    'She swallowed the spider to catch the fly.\n'
+}
+
+var verseSeven = function() {
+  return 'I know an old lady who swallowed a cow.\n' +
+    'I don\'t know how she swallowed a cow!\n' +
+    'She swallowed the cow to catch the goat.\n' +
+    'She swallowed the goat to catch the dog.\n' +
+    'She swallowed the dog to catch the cat.\n' +
+    'She swallowed the cat to catch the bird.\n' +
+    'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' +
+    'She swallowed the spider to catch the fly.\n'
+}
+
+var verseEight = function() {
+  return 'I know an old lady who swallowed a horse.\n' + 'She\'s dead, of course!\n'
+}
+
+var ending = function() {
+  return 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n'
+}
+
+module.exports = FoodChain
